18239743fc7b0db92947f59b46f5bca25f7d9aea
DESKTOP-2M6I01V\Kimmy Spencer <k.spencer97@ufl.edu>
2017-09-27 16:06:01 -0400

    intial commit

---
+++ src/catFace.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class catFace {
+    public static void main(String arg[]) {
+                System.out.println(" /\\---/\\");
+                System.out.println("(= o.o =)");
+        return;
+    }
+}
